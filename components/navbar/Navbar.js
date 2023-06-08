e8d3b9ba5c8cd79c5328a607d3f23eb47d9312b7
catherine <vuthi.esther@gmail.com>
2023-06-08 11:16:37 +0300

    add logo

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='border-b border-p-gray'>
-      <div className='navbar flex uppercase justify-between p-8 text-sm'>
-        <div>
-          <Link href='/'>NIALP</Link>
+      <div className='navbar flex uppercase justify-between px-8 py-5 text-sm'>
+        <div className='flex gap-2'>
+          <Image src='/assets/icons8-crane-48.png' alt='construction crane' width={48} height={48} />
+          <Link href='/' className='flex flex-col justify-center text-xl font-bold'>
+            NIALP
+          </Link>
         </div>
-        <div>
+        <div className='pt-3'>
           <ul className='flex gap-10'>
             <li className='hover:text-p-orange active:text-p-orange'>
               <Link href='/'>Home</Link>
@@ -17,7 +21,7 @@ const Navbar = () => {
               <Link href='/'>About</Link>
             </li>
             <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Services</Link>
+              <Link href='/'>Services</Link>git 
             </li>
             <li className='hover:text-p-orange active:text-p-orange'>
               <Link href='/'>Project</Link>
