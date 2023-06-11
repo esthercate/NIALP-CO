a9c9048801f68d43e24ccf81299308ede4e765d9
catherine <vuthi.esther@gmail.com>
2023-06-11 20:00:27 +0300

    change nav links design

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -3,9 +3,17 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const Navbar = () => {
+  let Links = [
+    { name: 'Home', link: '/' },
+    { name: 'About', link: '/' },
+    { name: 'Services', link: '/' },
+    { name: 'Project', link: '/' },
+    { name: 'Reviews', link: '/' },
+    { name: 'Contact', link: '/' },
+  ]
   return (
     <div className='border-b border-p-gray'>
-      <div className='navbar flex uppercase justify-between px-8 py-5 text-sm'>
+      <div className='navbar md:flex uppercase justify-between px-8 py-5 text-sm'>
         <div className='flex gap-2'>
           <Image src='/assets/icons8-crane-48.png' alt='construction crane' width={48} height={48} />
           <Link href='/' className='flex flex-col justify-center text-xl font-bold'>
@@ -13,25 +21,12 @@ const Navbar = () => {
           </Link>
         </div>
         <div className='pt-3'>
-          <ul className='flex gap-10'>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Home</Link>
-            </li>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>About</Link>
-            </li>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Services</Link>git 
-            </li>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Project</Link>
-            </li>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Reviews</Link>
-            </li>
-            <li className='hover:text-p-orange active:text-p-orange'>
-              <Link href='/'>Contact</Link>
-            </li>
+          <ul className='md:flex gap-10'>
+            {Links.map((link) => (
+              <li key={link.name} className='hover:text-p-orange active:text-p-orange'>
+                <Link href={link.link}>{ link.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
