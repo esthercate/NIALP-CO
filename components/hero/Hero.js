44bf3bc66cfdf6fda7a194bb386d9ee40b5796f1
catherine <vuthi.esther@gmail.com>
2023-06-12 23:42:35 +0300

    change height

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -4,7 +4,7 @@ import HeroSection from './component/hero-section/HeroSection'
 
 const Hero = () => {
   return (
-    <div className='w-full h-4/5 bg-white'>
+    <div className='w-full h-screen bg-white'>
       <HeroSection />
     </div>
   )
